c068d3f7018195d11b8fd9a3ee4dc60b3e683208
Dakota Taylor <dt900319@ohio.edu>
2021-04-22 22:48:05 -0400

    Added reasoning for test

--- tests/testcases.cpp
+++ tests/testcases.cpp
@@ -4,8 +4,8 @@
 #include "catch.hpp"
 
 TEST_CASE("Testing countChar()") {
-    CHECK(countChar("") == 0);
-    CHECK(countChar("12345") == 5);
-    CHECK(countChar("12\n34\t56") == 8);
-    CHECK(countChar("_^@") == 3);
+    CHECK(countChar("") == 0);            // Test for empty strings
+    CHECK(countChar("12345") == 5);       // A normal test
+    CHECK(countChar("12\n34\t56") == 8);  // A test for whitespaces
+    CHECK(countChar("_^@") == 3);         // A test for symbols
 }
